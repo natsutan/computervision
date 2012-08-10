9e705dd8f044182b964eebfef02244b49e53d55b
Minoru Natsutani <YRW03704@nifty.com>
2012-08-10 16:39:11 +0900

    ガウシアンの一階微分計算追加

--- PCV/chap1/gaussian_plot.py
+++ PCV/chap1/gaussian_plot.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 from pylab import *
+from scipy.misc import imsave
 
 xs = arange(-10.0, 10.0, 0.01)
 ys = []
 sigma = 2.0
 
 for x in xs:
-    y = (1/sqrt(2*pi*sigma*sigma)) * exp(-(x*x)/(2*sigma*sigma))
+#    y = (1/sqrt(2*pi*sigma*sigma)) * exp(-(x*x)/(2*sigma*sigma))
+    y = (x/(sqrt(2*pi)*sigma*sigma*sigma)) * exp(-(x*dx)/(2*sigma*sigma))
     ys.append(y)
 
 
 plot(xs,ys)
 
 show()
+
