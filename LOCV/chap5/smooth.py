fd6186cc076b1679aaa76c144c1e03f9c4288e19
Minoru Natsutani <YRW03704@nifty.com>
2015-05-09 22:09:31 +0900

    OpenCVを使ったブラー処理作成

--- LOCV/chap5/smooth.py
+++ LOCV/chap5/smooth.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
-from PIL import Image
+import cv2
 
-im = Image.open('../../image/twittan/twittan.jpg')
+src = cv2.imread('../../image/twittan/twittan.jpg')
+dst = src.copy()
 
-im.save('twi.jpg')
-im.show()
+roi_x = 100
+roi_y = 100
+roi_w = 150
+roi_h = 200
+
+dst[roi_y:roi_y+roi_w, roi_x:roi_x+roi_w] = cv2.blur(src[roi_y:roi_y+roi_w, roi_x:roi_x+roi_w], (5, 5), (-1, -1))
+cv2.imwrite('twi_blur_cv.jpg', dst)
+
+
 
