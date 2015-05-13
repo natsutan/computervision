3f8034551e81f8d48cc6b578d12e4b452c47602a
natsutan <YRW03704@nifty.com>
2015-05-13 19:20:11 +0900

    Dut top追加

--- LOCV/chap5/myhdl_top.py
+++ LOCV/chap5/myhdl_top.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'natu'
-from myhdl import Signal, delay, always, now, Simulation, instance, intbv, channel
-import smooth_hdl
+from myhdl import Signal, delay, always, now, Simulation, instance, intbv, traceSignals
+from smooth_hdl import smoother_top
 
 # paramter
 p_max_x = 1024
@@ -13,37 +13,47 @@ def run_sim():
 #    hello_inst = HelloWorld(clk)
 #    sim = Simulation(clkdriver_inst, hello_inst)
 
-    inst = env()
+    inst = traceSignals(env)
     sim = Simulation(inst)
 
-    sim.run(50)
+    sim.run(300)
 
 
 def env():
     # clk
-    clk = Signal(0)
-    uClkDriver = ClkDeriver(clk)
+    clk = Signal(bool(0))
+    uClkDriver = ClkDriver(clk)
+
+    reset = Signal(bool(0))
+    uResetDriver = ResetDriver(clk, reset)
 
     # input port
-    rin = Signal(intbv(0, min = 0, max = 255))
-    gin = Signal(intbv(0, min = 0, max = 255))
-    bin = Signal(intbv(0, min = 0, max = 255))
-    radr = Signal(intbv(0, min = 0, max = p_max_x * p_max_y))
-    inport_ch = channel(rin, gin, bin, radr)
+    rin = Signal(intbv(0, min=0, max=255))
+    gin = Signal(intbv(0, min=0, max=255))
+    bin = Signal(intbv(0, min=0, max=255))
+    radr = Signal(intbv(0, min=0, max=p_max_x * p_max_y))
 
     # output port
     rout = Signal(intbv(0, min = 0, max = 255))
     gout = Signal(intbv(0, min = 0, max = 255))
     bout = Signal(intbv(0, min = 0, max = 255))
     wadr = Signal(intbv(0, min = 0, max = p_max_x * p_max_y))
-    wen  = Signal(intbv(0))
-    outport_ch = channel(rout, gout, bout, wadr, wen)
+    wen  = Signal(intbv(bool(0)))
 
     # registers
-    reg =
+    reg_roi_x = Signal(intbv(0, min=0, max=p_max_x))
+    reg_roi_y = Signal(intbv(0, min=0, max=p_max_y))
+    reg_rot_w = Signal(intbv(0, min=0, max=p_max_x))
+    reg_rot_h = Signal(intbv(0, min=0, max=p_max_y))
 
+    uDut = smoother_top(
+        clk, reset,
+        rin, gin, bin, radr,
+        rout, gout, bout, wadr, wen,
+        reg_roi_x, reg_roi_y, reg_rot_h, reg_rot_w
+    )
 
-    return uClkDriver
+    return uClkDriver, uResetDriver
 
 
 
@@ -51,16 +61,16 @@ def greedings():
     clk1 = Signal(0)
     clk2 = Signal(0)
 
-    clkdriver1 = ClkDeriver(clk1)
-    clkdriver2 = ClkDeriver(clk = clk2, period=19)
+    clkdriver1 = ClkDriver(clk1)
+    clkdriver2 = ClkDriver(clk = clk2, period=19)
     hello1 = Hello(clk=clk1)
-    hello2 = Hello(to="MyHDL", clk = clk2)ｓ
+    hello2 = Hello(to="MyHDL", clk = clk2)
 
     return clkdriver1, clkdriver2, hello1, hello2
 
 
 
-def ClkDeriver(clk, period = 20):
+def ClkDriver(clk, period = 20):
     lowTime = int(period/2)
     highTime = period - lowTime
 
@@ -74,8 +84,30 @@ def ClkDeriver(clk, period = 20):
 
     return driveClk
 
-def Hello(clk, to="World!"):
 
+def ResetDriver(clk, reset):
+    lowTime = 2
+    highTime = 1
+
+    @instance
+    def driveReset():
+        reset.next = 0
+        for i in range(lowTime):
+            yield clk.posedge
+        reset.next = 1
+        for i in range(highTime):
+            yield clk.posedge
+        reset.next = 0
+        yield clk.posedge
+
+
+    return driveReset
+
+
+
+
+
+def Hello(clk, to="World!"):
 
     @always(clk.posedge)
     def sayHello():
--- LOCV/chap5/smooth_hdl.py
+++ LOCV/chap5/smooth_hdl.py
@@ -3,10 +3,18 @@ __author__ = 'natu'
 from myhdl import Signal, delay, always, now, Simulation, instance
 
 
-def smoother_top(clk, inport, outport, reg ):
+def smoother_top(
+        clk, reset,
+        rin, gin, bin, radr,
+        rout, gout, bout, wadr, wen,
+        reg_roi_x, reg_roi_y, reg_rot_h, reg_rot_w
+    ):
+
 
     def smoother():
         None
+
+
 
 
     return smoother
